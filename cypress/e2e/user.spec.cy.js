5a49e0d412e3b76fa8e119f0525c654a665f299f
Priscila-Sugano <priscila.sugano@hotmail.com>
2025-10-28 22:25:42 -0300

    User Update Success added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -15,11 +15,16 @@ const selectorsList = {
   genericField: ".oxd-input--active",
   dateField: "[placeholder='yyyy-dd-mm']",
   dateCloseButton: ".--close",
+  genericCombobox: ".oxd-select-text--after",
+  secondItemCombobox: '.oxd-select-dropdown > :nth-child(2) > span',
+  thirdItemCombobox: ':nth-child(3) > span',
+  genderCombobox: ".oxd-radio-wrapper",
   submitButton: "[type='submit']",
-
+  fourthItemCombobox: ".oxd-select-dropdown > :nth-child(6)",
+  
 }
 
-  it.only('User Info Update - Success', () => {
+  it('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
@@ -34,9 +39,22 @@ const selectorsList = {
     cy.get(selectorsList.genericField).eq(5).clear().type("DriverLicense") 
     cy.get(selectorsList.genericField).eq(6).clear().type("2025-23-10")
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get(selectorsList.genericCombobox).eq(0).click()
+    cy.get(selectorsList.secondItemCombobox).click()
+    cy.get(selectorsList.genericCombobox).eq(1).click()
+    cy.get(selectorsList.thirdItemCombobox).click()
+    cy.get(selectorsList.genericField).eq(8).clear().type("1990-29-09")
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genderCombobox).eq(1).click()
+    cy.get(selectorsList.submitButton).eq(0).click({force:true})
     cy.get("body").should("contain", "Successfully Updated")
-    cy.get('.oxd-toast-close')
+    cy.get('.oxd-toast-close').click()
+    cy.get(selectorsList.genericCombobox).eq(2).click()
+    cy.get(selectorsList.fourthItemCombobox).click()
+    cy.get(selectorsList.genericField).eq(9).clear().type("Test")
+    cy.get(selectorsList.submitButton).eq(1).click()
+    cy.get("body").should("contain", "Successfully Saved")
+    cy.get('.oxd-toast-close').click()
 
 
   })
